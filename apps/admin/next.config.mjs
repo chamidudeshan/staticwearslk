6da19d1971fac0bd685ebefedea6ca01ed06cbad
Chamidu Jayaneththi <chamidudeshanmn@gmail.com>
2026-06-09 21:17:20 +0530

    image domain added

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -8,6 +8,11 @@ const nextConfig = {
         hostname: '*.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.staticwears.com',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
   transpilePackages: [
--- apps/storefront/next.config.mjs
+++ apps/storefront/next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'api.staticwears.com',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
